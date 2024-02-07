d1f70038e667a32ad48bcd0bef5871c2b86a514f
Egor Zemlyanoy <egorz734@mail.ru>
2024-02-07 10:06:25 +0300

    switch url class to pydantic httpurl

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from pydantic import HttpUrl
 import random
 import string
 
-class URL(BaseModel):
-	url: str
 
 def make_short_url() -> str:
 	uniqueValues = string.ascii_letters + string.digits
@@ -19,7 +17,7 @@ async def read_root():
 	return "Home page!"
 
 @app.post("/shorturl")
-def shorten_url(url: URL):
+def shorten_url(url: HttpUrl):
 	shortURL = make_short_url()
 	linkDict[shortURL] = url.url
 	return {"short_url": shortURL}
