7692b1ac11442191aa4d3dfc46c3cd8c84ea5b32
Egor Zemlyanoy <egorz734@mail.ru>
2024-02-08 16:36:03 +0300

    add db (sqlite3)

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import HttpUrl
+from pydantic import BaseModel
+import sqlite3
 import random
 import string
 
+
+class URL(BaseModel):
+	url: str
 
 def make_short_url() -> str:
 	uniqueValues = string.ascii_letters + string.digits
 	return ''.join(random.choices(uniqueValues, k=6))
 
-linkDict = {}
+conn = sqlite3.connect('data/db.sqlite')
+try:
+	cursor = conn.cursor()
+	cursor.execute('CREATE TABLE urltable (shorturl VARCHAR(64) not null,'
+				'longurl VARCHAR(64) not null)')
+	conn.commit()
+except:
+	print('DB already exist!')
 
 app = FastAPI()
 
@@ -17,13 +28,22 @@ async def read_root():
 	return "Home page!"
 
 @app.post("/shorturl")
-def shorten_url(url: HttpUrl):
+def shorten_url(url: URL):
+	conn = sqlite3.connect('data/db.sqlite')
 	shortURL = make_short_url()
-	linkDict[shortURL] = url.url
+	cursor = conn.cursor()
+	cursor.execute(f'INSERT INTO urltable (shorturl, longurl) VALUES (\'{shortURL}\', \'{url.url}\')')
+	conn.commit()
 	return {"short_url": shortURL}
 
 @app.get("/shorturl/{shortURL}")
 def read_url(shortURL: str):
-	if shortURL not in linkDict.keys():
+	conn = sqlite3.connect('data/db.sqlite')
+	cursor = conn.cursor()
+	cursor.execute(f'SELECT * FROM urltable WHERE shorturl=\'{shortURL}\'')
+	resultList = cursor.fetchall()
+	if len(resultList) == 0:
 		raise HTTPException(status_code=404, detail="URL not found")
-	return linkDict[shortURL]
+	elif len(resultList) != 1:
+		raise HTTPException(status_code=404, detail="Multiple URLs were found")
+	return resultList
